0986dc5b21e6a9152569e1de8a99bd4b3e1b4ebf
luisandia <luisandiamamani@gmail.com>
2019-07-19 01:16:20 -0500

    add App blog

--- webEmpresa/webEmpresa/urls.py
+++ webEmpresa/webEmpresa/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('services/', include('services.urls'))
 ]
 
 if settings.DEBUG:
