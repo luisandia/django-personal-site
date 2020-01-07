d485c526d27a41f80a41fec998d3cebb4a6ac176
luisandia <luisandiamamani@gmail.com>
2020-01-06 22:55:20 -0500

    added contact

--- webEmpresa/core/views.py
+++ webEmpresa/core/views.py
@@ -13,8 +13,6 @@ def store(request):
     return render(request, "core/store.html")
 
 
-def contact(request):
-    return render(request, "core/contact.html")
 
 
 
--- webEmpresa/webEmpresa/settings.py
+++ webEmpresa/webEmpresa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'ckeditor',
+    'contact',
     'core',
     'services.apps.ServicesConfig',
     'social.apps.SocialConfig',
