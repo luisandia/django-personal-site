457cc95e69a5dd48fb4b482c5b0ad86bdf3fb952
luisandia <luisandiamamani@gmail.com>
2020-12-28 21:26:22 -0500

    added team data to  about and home page

--- webcarzone/pages/views.py
+++ webcarzone/pages/views.py
@@ -1,12 +1,34 @@
+from pages.models import Team
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+    # featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    # all_cars = Car.objects.order_by('-created_date')
+    # model_search = Car.objects.values_list('model', flat=True).distinct()
+    # city_search = Car.objects.values_list('city', flat=True).distinct()
+    # year_search = Car.objects.values_list('year', flat=True).distinct()
+    # body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    data = {
+        'teams': teams,
+        # 'featured_cars': featured_cars,
+        # 'all_cars': all_cars,
+        # 'model_search': model_search,
+        # 'city_search': city_search,
+        # 'year_search': year_search,
+        # 'body_style_search': body_style_search,
+    }
+    return render(request, 'pages/home.html', data)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/about.html', data)
+
 
 
 def services(request):
